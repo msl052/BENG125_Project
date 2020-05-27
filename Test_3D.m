0f118eb14e49682ec6eb04141d127b607f15b72d
Min Suk Lee <49286365+msl052@users.noreply.github.com>
2020-05-27 10:17:23 -0700

    Delete 3D graph

--- Test_3D.m
+++ Test_3D.m
@@ -1,6 +1,44 @@
-[x y] = meshgrid(-10:1:10);
-z = 2.*x + 3.*y + 4;
-surf(x,y,z);
+% z: I
+% x: F
+% y: A
+%
+initial.S = 499;	% set the initial value of 'S'
+initial.F = 100;    % set the initial value of 'F'
+initial.A = initial.S - initial.F; % set the initial value of 'A'
+initial.I = 1;	% set the initial value of 'I'
+initial.C = 1;
+initial.R = 0;	% set the initial value of 'R'
 
+%-------------------------------------------------------------------------
+% User Section 2: Definition of model parameters
+%-------------------------------------------------------------------------
+%
+% These parameters are passed to the function that calculates the
+% derivatives.
+%  
+% NOTE: Do not change the name 'param'!
+%
+param.beta = 1e-3; % set the parameter 'beta' of the model
+param.r = 1e-1;    % set the parameter 'r' of the model
+param.g = 3e-4;
+param.p =(param.beta*initial.S-param.g*initial.F)/initial.A;
+% This is the title string for the plot window.
+model_title = 'SIR Epidemics';
+
+%-------------------------------------------------------------------------
+% User Section 2: Plotting Nullclines
+%-------------------------------------------------------------------------
+%
+% F = r/p
+[y,z] = meshgrid(0:100:1000);
+x = repmat(param.r/param.g,size(z));
+surf(x,y,z);
 hold on;
-plot3(DataX, DataY, DataZ);
+[x,z] = meshgrid(0:10:1000);
+y = repmat(param.r/param.p,size(z));
+surf(x,y,z);
+xlabel('x:F');ylabel('y:A');zlabel('z:I')
+set(gca,'XLim',[0 1000]);
+set(gca,'YLim',[0 1000]);
+set(gca,'ZLim',[0 1000]);
+
